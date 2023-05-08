20b5c4bb6d2e2ad94a11580481de58d11cbd64f0
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-08 18:49:31 +0200

    Prune unnecessary imports for api/opengraph endpoint to reduce size

--- pages/api/opengraph.ts
+++ pages/api/opengraph.ts
@@ -1,8 +1,6 @@
 import { HOST } from "@/lib/opengraph";
 import * as playwright from "playwright-aws-lambda";
 import { NextApiRequest, NextApiResponse } from "next";
-import { isCustomErrorPage } from "next/dist/build/utils";
-import { NextRequest } from "next/server";
 import { ParsedUrlQuery } from "querystring";
 
 interface ExtendedQuery extends ParsedUrlQuery {
@@ -16,30 +14,22 @@ interface ExtendedRequest extends NextApiRequest {
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
   const { title } = req.query;
   let browser = null;
-
   try {
     browser = await playwright.launchChromium({ headless: true });
-
     const page = await browser.newPage({
       viewport: {
         width: 1200,
         height: 630,
       },
     });
-
     const url = `${HOST}/opengraph?title=${title}`;
-
     await page.goto(url);
-
     const data = await page.getByTestId("opengraph-img").screenshot({
       type: "png",
     });
-
     await browser.close();
-
     res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
     res.setHeader("Content-Type", "image/png");
-
     res.end(data);
   } catch (error) {
     throw error;
