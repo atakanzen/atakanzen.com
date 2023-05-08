c9f4f19ec838b7d36c26068e1d90961070a39db4
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-08 18:52:43 +0200

    Prune api/opengraph again

--- pages/api/opengraph.ts
+++ pages/api/opengraph.ts
@@ -13,27 +13,22 @@ interface ExtendedRequest extends NextApiRequest {
 
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
   const { title } = req.query;
-  let browser = null;
-  try {
-    browser = await playwright.launchChromium({ headless: true });
-    const page = await browser.newPage({
-      viewport: {
-        width: 1200,
-        height: 630,
-      },
-    });
-    const url = `${HOST}/opengraph?title=${title}`;
-    await page.goto(url);
-    const data = await page.getByTestId("opengraph-img").screenshot({
-      type: "png",
-    });
-    await browser.close();
-    res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
-    res.setHeader("Content-Type", "image/png");
-    res.end(data);
-  } catch (error) {
-    throw error;
-  }
+  const browser = await playwright.launchChromium({ headless: true });
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 630,
+    },
+  });
+  const url = `${HOST}/opengraph?title=${title}`;
+  await page.goto(url);
+  const data = await page.getByTestId("opengraph-img").screenshot({
+    type: "png",
+  });
+  await browser.close();
+  res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate");
+  res.setHeader("Content-Type", "image/png");
+  res.end(data);
 };
 
 export default handler;
