50d95a12a8c5aa6917a9ad50a7556d73a0eff3b6
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-31 20:54:54 +0200

    Add margin-x on opengraph dynamic page

--- pages/api/opengraph.tsx
+++ pages/api/opengraph.tsx
@@ -29,8 +29,8 @@ const handler = async (req: NextRequest) => {
           }}
           tw="flex items-center justify-center bg-white w-full h-full"
         >
-          <div tw="flex flex-col items-center justify-center w-full">
-            <p tw="text-6xl font-black text-center">{title}</p>
+          <div tw="flex flex-col items-center justify-center">
+            <p tw="text-6xl font-black text-center mx-36">{title}</p>
             <a tw="text-3xl ">atakanzen.com</a>
           </div>
         </div>
