672a2c51c65d2a6870c6320bd8e4556788971f02
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-31 16:25:15 +0200

    Add v2 param to force favicon cache dismissal on google search

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
-const gtagId = process.env.GTAG_ID;
-
 export default function Document() {
   return (
     <Html lang="en">
@@ -10,21 +8,21 @@ export default function Document() {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/apple-touch-icon.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon-32x32.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon-16x16.png?v2"
         />
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=2" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
