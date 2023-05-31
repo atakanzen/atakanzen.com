4a5c84df12ff70c63846bae82e6586027300ce16
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-31 14:24:35 +0200

    Add Google Tag script for analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+const gtagId = process.env.GTAG_ID;
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtagId}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtagId}');
+      `}
+        </Script>
+
         <link
           rel="apple-touch-icon"
           sizes="180x180"
