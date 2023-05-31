c4cf7dad1c599b4a2992cf2be88784770f22b3af
Atakan Zengin <atakanzzengin@gmail.com>
2023-05-31 15:44:58 +0200

    Change temporary domain to valid one throughout the app

--- lib/opengraph.ts
+++ lib/opengraph.ts
@@ -1,7 +1,7 @@
 export const HOST =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://new.atakanzen.com";
+    : "https://atakanzen.com";
 
 export const getOpenGraphImage = (title: string) => {
   return {
