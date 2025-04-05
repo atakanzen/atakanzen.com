6fda23bd0a29ba8bbe215cfef5d753c6af84df25
Atakan Zengin <atakanzzengin@gmail.com>
2025-04-05 20:25:45 +0200

    refactor: 💡 Use outstatic API to get post information

--- app/(site)/writing/[slug]/layout.tsx
+++ app/(site)/writing/[slug]/layout.tsx
@@ -1,4 +1,5 @@
-import { getPostMetadata } from "@/lib/mdx";
+import { POSTS_COLLECTION } from "@/lib/constants";
+import { getDocumentBySlug } from "outstatic/server";
 
 export default async function MdxLayout({
   children,
@@ -8,12 +9,16 @@ export default async function MdxLayout({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  const { matter } = await getPostMetadata(`${slug}.mdx`);
+  const post = getDocumentBySlug(POSTS_COLLECTION, slug, ["publishedAt"]);
+  if (!post) {
+    throw new Error("No post found");
+  }
+  const publishedAt = new Date(post.publishedAt).toDateString();
 
   return (
     <div className="flex flex-col justify-center py-8 gap-4 xs:max-w-xs sm:max-w-xl md:max-w-2xl xl:max-w-full xl:items-center mx-auto">
       <span className="text-blue-500 text-base md:text-xl font-bold">
-        {matter.date}
+        {publishedAt}
       </span>
       <article className="prose xl:prose-xl 2xl:prose-2xl prose-blue pt-4 dark:prose-invert prose-a:underline xs:prose-pre:max-w-sm sm:prose-pre:max-w-md md:prose-pre:max-w-none prose-a:text-blue-500 prose-pre:bg-[#0D1117]">
         {children}
