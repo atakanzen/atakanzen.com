f0ed7b14a060b471d0d1b999d8199217343f72c3
Atakan Zengin <atakanzzengin@gmail.com>
2025-04-05 18:44:13 +0200

    feat: 🎸 Add outstatic packages and split app into route groups

---
+++ app/(cms)/layout.tsx
@@ -0,0 +1,12 @@
+import "outstatic/outstatic.css";
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body id="outstatic">{children}</body>
+    </html>
+  );
+}
