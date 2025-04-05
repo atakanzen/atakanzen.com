cc0bf1a66c909992ccea5d4a3999116f3862aab4
Atakan Zengin <atakanzzengin@gmail.com>
2025-04-05 22:16:02 +0200

    feat: 🎸 Add rehype-callouts stylings and plugin

--- app/(site)/writing/[slug]/layout.tsx
+++ app/(site)/writing/[slug]/layout.tsx
@@ -13,14 +13,19 @@ export default async function MdxLayout({
   if (!post) {
     throw new Error("No post found");
   }
-  const publishedAt = new Date(post.publishedAt).toDateString();
+  const publishedAt = new Date(post.publishedAt).toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
   return (
     <div className="flex flex-col justify-center py-8 gap-4 xs:max-w-xs sm:max-w-xl md:max-w-2xl xl:max-w-full xl:items-center mx-auto">
-      <span className="text-blue-500 text-base md:text-xl font-bold">
+      <span className="text-gray-500 text-base md:text-xl font-bold">
         {publishedAt}
       </span>
-      <article className="prose xl:prose-xl 2xl:prose-2xl prose-blue pt-4 dark:prose-invert prose-a:underline xs:prose-pre:max-w-sm sm:prose-pre:max-w-md md:prose-pre:max-w-none prose-a:text-blue-500 prose-pre:bg-[#0D1117]">
+      <article className="prose xl:prose-xl 2xl:prose-2xl prose-blue pt-4 dark:prose-invert prose-a:underline xs:prose-pre:max-w-sm sm:prose-pre:max-w-md md:prose-pre:max-w-none prose-a:text-blue-500 prose-pre:bg-[#0D1117] dark">
         {children}
       </article>
     </div>
